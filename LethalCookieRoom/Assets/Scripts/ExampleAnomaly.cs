1daa66de7c37ae16ef9059d55e4c3d9b5fd60b83
Arthur Wang <91100208+Tzanccc@users.noreply.github.com>
2024-02-29 15:30:49 -0600

    Feature/anomaly system (#2)
    * Rewrote state machine states, implemented state transitions, expanded state machine functionality to include onEnter/onExit functions
    
    * Refactored state machine to use inheritance
    
    * Completed first version of anomaly system
    
    * Removed comments
    
    * Moved timeout functions around
    
    * Renamed meta file

---
+++ LethalCookieRoom/Assets/Scripts/ExampleAnomaly.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestAnomaly : AnomalyStateMachine
+{
+    int timeoutTriggerSeconds;
+    int anomalyTriggerSeconds;
+
+    void Start()
+    {
+        initStateMachine();
+        timeoutTriggerSeconds = 5;
+        anomalyTriggerSeconds = 10;
+        TriggerEvent(AnomalyEvent.ResponseTriggered);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Debug.Log("Triggering response event");
+            TriggerEvent(AnomalyEvent.ResponseTriggered);
+        }
+    }
+
+    protected override void onIdleEnter(AnomalyEvent anomalyEvent)
+    {
+        Debug.Log($"Entering state Idle from event {anomalyEvent}");
+    }
+
+    protected override void onIdleExit(AnomalyEvent anomalyEvent)
+    {
+        Debug.Log($"Leaving state Idle from event {anomalyEvent}");
+        if (anomalyEvent == AnomalyEvent.QueueAnomaly)
+        {
+            StartCoroutine(TimerTriggerAnomaly(anomalyTriggerSeconds));
+        }
+    }
+
+    protected override void onQueuedEnter(AnomalyEvent anomalyEvent)
+    {
+        Debug.Log($"Entering state Queued from event {anomalyEvent}");
+    }
+
+    protected override void onQueuedExit(AnomalyEvent anomalyEvent)
+    {
+        Debug.Log($"Leaving state Queued from event {anomalyEvent}");
+        if (anomalyEvent == AnomalyEvent.ResponseTriggered)
+        {
+            Debug.Log(" - Penaulty triggered from spamming response");
+        }
+    }
+
+    protected override void onActiveEnter(AnomalyEvent anomalyEvent)
+    {
+        Debug.Log($"Entering state Active from event {anomalyEvent}");
+        StartCoroutine(TimerTriggerTimeout(timeoutTriggerSeconds));
+    }
+
+    protected override void onActiveExit(AnomalyEvent anomalyEvent)
+    {
+        Debug.Log($"Leaving state Active from event {anomalyEvent}");
+        if (anomalyEvent == AnomalyEvent.ResponseTriggered)
+        {
+            
+        }
+        else if (anomalyEvent == AnomalyEvent.TimeoutTriggered)
+        {
+            Debug.Log(" - Penaulty triggered from timeout");
+        }
+        StartCoroutine(TimerTriggerAnomaly(anomalyTriggerSeconds));
+    }
+}
