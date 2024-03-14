ee1bf482fc92af17aede458d4ffb8261ed019aef
Wyatt <49281598+zapfeeno@users.noreply.github.com>
2024-03-14 14:50:27 -0500

    change camera

--- LethalCookieRoom/Assets/Scripts/CameraManager.cs
+++ LethalCookieRoom/Assets/Scripts/CameraManager.cs
@@ -8,28 +8,37 @@ public class CameraManager : MonoBehaviour
     public GameObject screen;
     public RenderTexture cam1;
     public RenderTexture cam2;
+    public RenderTexture cam3;
 
     private int active = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        screen.GetComponent<Renderer>().material.SetTexture("_EmissionMap", cam1);
+        screen.GetComponent<Renderer>().material.SetTexture("_Base", cam1);
         active = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown("space") && active != 2)
+        if (Input.GetKeyDown("space"))
         {
-            screen.GetComponent<Renderer>().material.SetTexture("_EmissionMap", cam2);
-            active = 2;
+            switch(active) {
+
+                case 1:
+                    screen.GetComponent<Renderer>().material.SetTexture("_Base", cam2);
+                    active = 2;
+                case 2: 
+                    screen.GetComponent<Renderer>().material.SetTexture("_Base", cam3);
+                    active = 3;
+                case 3: 
+                    screen.GetComponent<Renderer>().material.SetTexture("_Base", cam1);
+                    active = 1;
+
+            }
         }
-        else if (Input.GetKeyDown("space") && active != 1)
-        {
-            screen.GetComponent<Renderer>().material.SetTexture("_EmissionMap", cam1);
-            active = 1;
-        }
+
+
     }
 }
