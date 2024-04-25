569b9aa1ab28e2ad059567dae24f01f41fd9e25f
Wyatt <49281598+zapfeeno@users.noreply.github.com>
2024-04-25 15:27:47 -0500

    Timer groundwork and interact text fix

--- LethalCookieRoom/Assets/Scripts/ManagerScripts/OverlayGuide.cs
+++ LethalCookieRoom/Assets/Scripts/ManagerScripts/OverlayGuide.cs
@@ -10,6 +10,11 @@ public class OverlayGuide : MonoBehaviour
     public GameObject overlayGuide;
     private GameObject InteractGuide;
     private GameObject MonitorGuide;
+    private GameObject timeDisplay;
+    private TextMeshProUGUI timeText;
+
+    public static float startTime = 60.0f * 10;
+    public static float timePassed = 0.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -17,12 +22,23 @@ public class OverlayGuide : MonoBehaviour
         overlayGuide = GameObject.Find("OverlayGuide");
         InteractGuide = GameObject.Find("InteractGuide");
         MonitorGuide = GameObject.Find("MonitorGuide");
+        timeDisplay = GameObject.Find("timeDisplay");
+        timeText = timeDisplay.GetComponentInChildren<TextMeshProUGUI>();
 
         InteractGuide.SetActive(false);
         MonitorGuide.SetActive(false);
+        timeDisplay.SetActive(false);
     }
 
-    // called in interactioncontrol script
+    // Update is called once per frame
+    void Update()
+    {
+        timePassed = startTime - Time.time;
+
+    }
+
+    // activated in interactioncontrol script
+    // deactivated in emissioncontrol
     public void showInteractGuide(bool active) {
         if(active) {   
             // set text from keymanager
@@ -38,9 +54,4 @@ public class OverlayGuide : MonoBehaviour
         MonitorGuide.SetActive(active);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
 }
