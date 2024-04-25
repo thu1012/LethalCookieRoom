1433decfd34cb21c9744d8060737c078b41be0ac
Arthur <arwang5@wisc.edu>
2024-04-25 14:27:05 -0500

    Package anomaly starter code

---
+++ LethalCookieRoom/Assets/Scripts/Anomaly/PackageAnomaly.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+
+public class PackageAnomaly : AnomalyStateMachine
+{
+    public GameObject package;
+    public GameObject responseObject;
+    private ResponseControl responseControl;
+
+    private void Start() {
+        initStateMachine();
+        responseControl = responseObject.GetComponent<ResponseControl>();
+        package.SetActive(false);
+        TriggerEvent(AnomalyEvent.QueueAnomaly);
+    }
+
+    protected override void onIdleEnter(AnomalyEvent anomalyEvent) {
+        Debug.Log($"Entering state Idle from event {anomalyEvent}");
+    }
+
+    protected override void onIdleExit(AnomalyEvent anomalyEvent) {
+        Debug.Log($"Leaving state Idle from event {anomalyEvent}");
+        if (anomalyEvent == AnomalyEvent.QueueAnomaly) {
+            currentCoroutine = timerTriggerAnomaly(waitForCameraSwitchAway());
+            StartCoroutine(currentCoroutine);
+        }
+    }
+
+    protected override void onQueuedEnter(AnomalyEvent anomalyEvent) {
+        Debug.Log($"Entering state Queued from event {anomalyEvent}");
+    }
+
+    protected override void onQueuedExit(AnomalyEvent anomalyEvent) {
+        Debug.Log($"Leaving state Queued from event {anomalyEvent}");
+        if (anomalyEvent == AnomalyEvent.ResponseTriggered) {
+            Debug.Log(" - Penaulty triggered from spamming response");
+            sanityControl.decreaseSanity(sanityPenalty);
+        }
+    }
+
+    protected override void onActiveEnter(AnomalyEvent anomalyEvent) {
+        Debug.Log($"Entering state Active from event {anomalyEvent}");
+        package.SetActive(true);
+        currentCoroutine = timerTriggerTimeout();
+        StartCoroutine(currentCoroutine);
+        warningCoroutine = timerTriggerAlarm();
+        StartCoroutine(warningCoroutine);
+        responseControl.onAnomalyStart(1);
+    }
+
+    protected override void onActiveExit(AnomalyEvent anomalyEvent) {
+        Debug.Log($"Leaving state Active from event {anomalyEvent}");
+        package.SetActive(false);
+        StopCoroutine(currentCoroutine);
+        StopCoroutine(warningCoroutine);
+        anomalyWarning.updateAlarmLevel(0);
+        if (anomalyEvent == AnomalyEvent.ResponseTriggered) {
+
+        } else if (anomalyEvent == AnomalyEvent.TimeoutTriggered) {
+            Debug.Log(" - Penaulty triggered from timeout");
+            sanityControl.decreaseSanity(sanityPenalty);
+        }
+        currentCoroutine = timerTriggerAnomaly(waitForCameraSwitchAway());
+        StartCoroutine(currentCoroutine);
+    }
+}
