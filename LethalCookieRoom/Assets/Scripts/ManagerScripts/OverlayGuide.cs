4eb0cf794d8c914e60f1bbe3bb5802af2b84a459
Wyatt <49281598+zapfeeno@users.noreply.github.com>
2024-04-25 16:13:32 -0500

    Adding text and timer

--- LethalCookieRoom/Assets/Scripts/ManagerScripts/OverlayGuide.cs
+++ LethalCookieRoom/Assets/Scripts/ManagerScripts/OverlayGuide.cs
@@ -9,7 +9,9 @@ public class OverlayGuide : MonoBehaviour
 
     public GameObject overlayGuide;
     private GameObject InteractGuide;
+    private TextMeshProUGUI interactText;
     private GameObject MonitorGuide;
+    private TextMeshProUGUI monitorText;
     private GameObject timeDisplay;
     private TextMeshProUGUI timeText;
 
@@ -21,20 +23,42 @@ public class OverlayGuide : MonoBehaviour
     {
         overlayGuide = GameObject.Find("OverlayGuide");
         InteractGuide = GameObject.Find("InteractGuide");
+        interactText = InteractGuide.GetComponentInChildren<TextMeshProUGUI>();
         MonitorGuide = GameObject.Find("MonitorGuide");
+        monitorText = MonitorGuide.GetComponentInChildren<TextMeshProUGUI>();
         timeDisplay = GameObject.Find("timeDisplay");
         timeText = timeDisplay.GetComponentInChildren<TextMeshProUGUI>();
 
         InteractGuide.SetActive(false);
         MonitorGuide.SetActive(false);
-        timeDisplay.SetActive(false);
+        //timeDisplay.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        timePassed = startTime - Time.time;
+        timePassed = Time.time;
+        //Debug.Log(timePassed);
 
+        if (timePassed >= 60 * 10) {
+            timeText.text = "you win";
+            // do something here
+        }
+        else if (timePassed >= 60 * 8) {
+            timeText.text = "4am";
+        }
+        else if (timePassed >= 60 * 6) {
+            timeText.text = "3am";
+        }
+        else if (timePassed >= 60 * 4) {
+            timeText.text = "2am";
+        }
+        else if (timePassed >= 60 * 2) {
+            timeText.text = "1am";
+        }
+        else {
+            timeText.text = "12am";
+        }
     }
 
     // activated in interactioncontrol script
@@ -42,6 +66,7 @@ public class OverlayGuide : MonoBehaviour
     public void showInteractGuide(bool active) {
         if(active) {   
             // set text from keymanager
+            interactText.text = PlayerPrefs.GetString("Interact") + " to interact";
         }
         InteractGuide.SetActive(active);
     }
@@ -49,7 +74,7 @@ public class OverlayGuide : MonoBehaviour
     // called in MonitorControl script
     public void showMonitorGuide(bool active) {
         if(active) {   
-            // set text from keymanager
+            monitorText.text = PlayerPrefs.GetString("CameraSwitch") + " to change camera\n" + PlayerPrefs.GetString("ExitInteract") + " to stand up";
         }
         MonitorGuide.SetActive(active);
     }
