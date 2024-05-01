3aedf2dd0014b4e339f358bf885657c7dc74ff15
Wyatt <49281598+zapfeeno@users.noreply.github.com>
2024-05-01 17:45:28 -0500

    UI Button sounds!

--- LethalCookieRoom/Assets/Scripts/ManagerScripts/LoreScreen.cs
+++ LethalCookieRoom/Assets/Scripts/ManagerScripts/LoreScreen.cs
@@ -7,12 +7,16 @@ public class LoreScreen : MonoBehaviour
 {
     public Button applyNowButton;
     public SceneChanger sceneChanger;
+    public AudioSource audioSource;
 
     // Start is called before the first frame update
     void Start()
     {
         sceneChanger = GameObject.FindObjectOfType<SceneChanger>();
         applyNowButton.onClick.AddListener(goToGame);
+        if(audioSource == null) {
+            audioSource = gameObject.AddComponent<AudioSource>();
+        }
     }
 
     // Update is called once per frame
@@ -23,5 +27,10 @@ public class LoreScreen : MonoBehaviour
 
     void goToGame() {
         sceneChanger.goToScene("Main Scene 0409");
+    }
+
+    public void playAudio(AudioClip sound) {
+        audioSource.clip = sound;
+        audioSource.Play();
     }
 }
