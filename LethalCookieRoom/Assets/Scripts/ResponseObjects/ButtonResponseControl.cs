b2a51399bcc4b5aaae71016daee6f55ac1c6565c
Tairan Hu <tairan1012@gmail.com>
2024-05-02 00:05:43 -0500

    buildFixes

--- LethalCookieRoom/Assets/Scripts/ResponseObjects/ButtonResponseControl.cs
+++ LethalCookieRoom/Assets/Scripts/ResponseObjects/ButtonResponseControl.cs
@@ -1,6 +1,4 @@
 ﻿using UnityEngine;
-using System.Collections;
-using UnityEditor.Animations;
 
 public class ButtonResponseControl : ResponseControl {
     public override void triggerClickResponse() {
--- LethalCookieRoom/Assets/Scripts/ResponseObjects/DialResponseControl.cs
+++ LethalCookieRoom/Assets/Scripts/ResponseObjects/DialResponseControl.cs
@@ -1,6 +1,4 @@
 ﻿using UnityEngine;
-using System.Collections;
-using UnityEditor.Animations;
 
 public class DialResponseControl : ResponseControl {
     public override void triggerClickResponse() {
