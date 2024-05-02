28cd6734fae6047b22b5059beabd620471250080
Wyatt <49281598+zapfeeno@users.noreply.github.com>
2024-05-01 23:24:43 -0500

    fixed winning/losing null pointers and removed cursor lock on refocus

--- LethalCookieRoom/Assets/Scripts/ManagerScripts/Losing.cs
+++ LethalCookieRoom/Assets/Scripts/ManagerScripts/Losing.cs
@@ -14,7 +14,8 @@ public class Losing : MonoBehaviour
         //winAnimation = this.gameObject.GetComponent<Animation>();
         sceneChanger = GameObject.FindObjectOfType<SceneChanger>();
         loseButton.onClick.AddListener(goToMainMenu);
-        this.gameObject.SetActive(false);
+        // this is now set inactive in overlayguide to prevent concurrency issues
+        //this.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
